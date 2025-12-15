726b1821be3d880812810242b1b74f7139661d7a
Joso1207 <joso1207@student.miun.se>
2025-12-15 13:30:02 +0100

    Student Entity

---
+++ src/main/java/se/chasacademy/databaser/repositories/StudentRepository.java
@@ -0,0 +1,12 @@
+package se.chasacademy.databaser.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.chasacademy.databaser.jpastart.models.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+
+}
