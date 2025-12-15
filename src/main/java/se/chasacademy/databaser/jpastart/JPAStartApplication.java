6bb8dff69e77238a0c1ffb33847762e83207a29d
Joso1207 <joso1207@student.miun.se>
2025-12-15 13:44:04 +0100

    Start application initialize repos

--- src/main/java/se/chasacademy/databaser/jpastart/JPAStartApplication.java
+++ src/main/java/se/chasacademy/databaser/jpastart/JPAStartApplication.java
@@ -3,9 +3,26 @@ package se.chasacademy.databaser.jpastart;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.chasacademy.databaser.jpastart.repositories.CourseRepository;
+import se.chasacademy.databaser.jpastart.repositories.StudentRepository;
+import se.chasacademy.databaser.jpastart.repositories.TeacherRepository;
 
 @SpringBootApplication
 public class JPAStartApplication implements CommandLineRunner {
+
+    CourseRepository courseRepository;
+    StudentRepository studentRepository;
+    TeacherRepository teacherRepository;
+
+    JPAStartApplication(
+            CourseRepository courseRepository,
+            StudentRepository studentRepository,
+            TeacherRepository teacherRepository
+    ){
+        this.courseRepository = courseRepository;
+        this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
+    }
 
 
 	public static void main(String[] args) {
