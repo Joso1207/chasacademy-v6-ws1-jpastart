7508f8264bc236592f179a4fdb802eb9516042b9
Oliver Öhrström <xzereha@live.se>
2025-12-15 13:57:57 +0100

    Teacher course mapping

--- src/main/java/se/chasacademy/databaser/jpastart/models/Teacher.java
+++ src/main/java/se/chasacademy/databaser/jpastart/models/Teacher.java
@@ -1,10 +1,14 @@
 package se.chasacademy.databaser.jpastart.models;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Teacher {
@@ -15,5 +19,44 @@ public class Teacher {
     private String name;
     @Column(nullable = false)
     private String email;
+    @OneToMany(mappedBy = "teacher", fetch = FetchType.EAGER)
+    private Set<Course> courses;
 
+    public Teacher() {
+    }
+
+    public Teacher(long id, String name, String email, Set<Course> courses) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.courses = courses;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
 }
